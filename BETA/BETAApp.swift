03018faee3e1eb87510b498f519e44cf1ab7deaa
Ryan RK <rk_ryan@icloud.com>
2021-11-05 12:29:00 +0800

    Initial Commit

---
+++ BETA/BETAApp.swift
@@ -0,0 +1,17 @@
+//
+//  BETAApp.swift
+//  BETA
+//
+//  Created by Ryan RK on 5/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct BETAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
